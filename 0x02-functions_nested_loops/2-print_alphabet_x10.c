cde7c93ed2d2a5e63070512624d74c337ac8e29b
CloudKing2023 <josephigwe1990@gmail.com>
2023-08-21 12:18:45 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10  - Print x10 the alphabets
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'a'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
