2d2cd348959c3ef23ed7267d683b0ea1ce39f638
CloudKing2023 <josephigwe1990@gmail.com>
2023-08-22 20:25:41 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number 0 to 9
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
